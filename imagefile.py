2ed91be4e04b83dcf5cf2327c2959229053659fe
andkar <karrestad98@live.no>
2023-01-17 17:00:26 +0100

    imagefile

---
+++ imagefile.py
@@ -0,0 +1,9 @@
+import requests
+import os
+import io
+
+response = requests.get("https://picsum.photos/200/300")
+my_file = open("image.jpg","wb")
+my_file.write(response.content)
+my_file.close()
+os.system("open image.jpg")
